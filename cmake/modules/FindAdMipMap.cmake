3b3bbdfe8276ed6976ed8578c6f153c6f6a03236
Matus Novak <email@matusnovak.com>
2023-07-16 00:26:47 +0200

    Fix FindAdMipMap

--- cmake/modules/FindAdMipMap.cmake
+++ cmake/modules/FindAdMipMap.cmake
@@ -14,7 +14,7 @@ file(GLOB_RECURSE SOURCES
         ${CMAKE_CURRENT_BINARY_DIR}/third_party/SimplexNoise/src/*.hpp
         ${CMAKE_CURRENT_BINARY_DIR}/third_party/SimplexNoise/src/*.h)
 
-add_library(AdMipMap STATIC ${SOURCES})
+add_library(AdMipMap INTERFACE ${SOURCES})
 add_library(AdMipMap::AdMipMap ALIAS AdMipMap)
 set_target_properties(AdMipMap PROPERTIES C_STANDARD 11)
 set_target_properties(AdMipMap PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_BINARY_DIR}/third_party/ad_mipmap")
