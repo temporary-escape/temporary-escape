8abf67d6f6ecad3924bc6c9678f7bbcbf499ef1b
Matus Novak <email@matusnovak.com>
2023-08-25 01:03:35 +0200

    Add xml parser

---
+++ src/engine/utils/xml.hpp
@@ -0,0 +1,437 @@
+#pragma once
+
+#include "../library.hpp"
+#include "../math/quaternion.hpp"
+#include "../math/vector.hpp"
+#include "macros.hpp"
+#include "moveable_copyable.hpp"
+#include "path.hpp"
+#include <charconv>
+#include <list>
+#include <unordered_map>
+#include <vector>
+
+// Forward declaration
+typedef struct _xmlDoc xmlDoc;
+typedef xmlDoc* xmlDocPtr;
+typedef struct _xmlNode xmlNode;
+typedef xmlNode* xmlNodePtr;
+
+namespace Engine::Xml {
+
+class Node;
+
+template <typename T> void convert(const Node& node, const std::string& key, T& value, bool required = true);
+template <typename T> void pack(Node& node, const std::string& key, const T& value);
+
+class Node {
+public:
+    Node() = default;
+    explicit Node(const Node* parent, const std::string& name);
+    explicit Node(const Node* parent, xmlNodePtr node);
+    ~Node();
+    Node(const Node& other) = delete;
+    Node(Node&& other) noexcept;
+    Node& operator=(const Node& other) = delete;
+    Node& operator=(Node&& other) noexcept;
+    void swap(Node& other);
+
+    [[nodiscard]] xmlNodePtr get() const {
+        return node;
+    }
+
+    template <typename T> void pack(const std::string& key, const T& value) {
+        Xml::pack(*this, key, value);
+    }
+
+    template <typename T> void convert(const std::string& key, T& value, bool required = true) const {
+        Xml::convert(*this, key, value, required);
+    }
+
+    Node insert(const std::string& key);
+    void setText(const std::string& text);
+    [[nodiscard]] std::string_view name() const;
+    [[nodiscard]] std::string_view getText() const;
+    [[nodiscard]] Node next() const;
+    [[nodiscard]] Node next(const std::string& name) const;
+    [[nodiscard]] Node child() const;
+    [[nodiscard]] Node child(const std::string& name) const;
+    [[nodiscard]] bool empty() const;
+
+    operator bool() const {
+        return parent && node;
+    }
+
+    [[nodiscard]] std::string breadcrumbs() const;
+    void throwWith(const std::string_view& msg) const {
+        throw std::runtime_error(fmt::format("Unable to parse property: {} error: {}", breadcrumbs(), msg));
+    }
+
+private:
+    const Node* parent{nullptr};
+    xmlNodePtr node{nullptr};
+    mutable unsigned char* contents{nullptr};
+};
+
+class Document {
+public:
+    explicit Document(const std::string& raw);
+    explicit Document(const std::string& rootName, const std::string& version);
+    ~Document();
+    NON_COPYABLE(Document);
+    MOVEABLE(Document);
+
+    [[nodiscard]] std::string toString() const;
+    Node& getRoot() {
+        return root;
+    }
+    [[nodiscard]] const Node& getRoot() const {
+        return root;
+    }
+
+private:
+    xmlDocPtr doc{nullptr};
+    Node root;
+};
+
+namespace Detail {
+template <typename T> struct DocumentDefinition;
+} // namespace Detail
+
+template <typename T> struct Adaptor {
+    // Pack adaptor for all user defined types
+    static void pack(Node& node, const T& value) {
+        value.pack(node);
+    }
+    // Convert adaptor for all user defined types
+    static void convert(const Node& node, T& value) {
+        value.convert(node);
+    }
+};
+
+template <> struct Adaptor<std::string> {
+    static void pack(Node& node, const std::string& value) {
+        node.setText(value);
+    }
+    static void convert(const Node& node, std::string& value) {
+        value = node.getText();
+    }
+};
+
+template <> struct Adaptor<bool> {
+    static void pack(Node& node, const bool& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, bool& value) {
+        const auto text = node.getText();
+        if (text.empty()) {
+            node.throwWith("empty bool value");
+        } else if (text != "true" && text != "false") {
+            node.throwWith(R"(invalid bool value, expected "true" or "false")");
+        }
+        value = text == "true";
+    }
+};
+
+template <> struct Adaptor<float> {
+    static void pack(Node& node, const float& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, float& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid float value");
+        }
+    }
+};
+
+template <> struct Adaptor<double> {
+    static void pack(Node& node, const double& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, double& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid double value");
+        }
+    }
+};
+
+template <> struct Adaptor<int32_t> {
+    static void pack(Node& node, const int32_t& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, int32_t& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid int32_t value");
+        }
+    }
+};
+
+template <> struct Adaptor<uint32_t> {
+    static void pack(Node& node, const uint32_t& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, uint32_t& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid uint32_t value");
+        }
+    }
+};
+
+template <> struct Adaptor<int64_t> {
+    static void pack(Node& node, const int64_t& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, int64_t& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid int64_t value");
+        }
+    }
+};
+
+template <> struct Adaptor<uint64_t> {
+    static void pack(Node& node, const uint64_t& value) {
+        node.setText(fmt::format("{}", value));
+    }
+    static void convert(const Node& node, uint64_t& value) {
+        const auto text = node.getText();
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+        if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+            node.throwWith("invalid uint64_t value");
+        }
+    }
+};
+
+template <> struct Adaptor<Vector2> {
+    static void pack(Node& node, const Vector2& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+    }
+    static void convert(const Node& node, Vector2& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+    }
+};
+
+template <> struct Adaptor<Vector2i> {
+    static void pack(Node& node, const Vector2i& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+    }
+    static void convert(const Node& node, Vector2i& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+    }
+};
+
+template <> struct Adaptor<Vector3> {
+    static void pack(Node& node, const Vector3& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+        node.pack("z", value.z);
+    }
+    static void convert(const Node& node, Vector3& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+        node.convert("z", value.z);
+    }
+};
+
+template <> struct Adaptor<Vector3i> {
+    static void pack(Node& node, const Vector3i& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+        node.pack("z", value.z);
+    }
+    static void convert(const Node& node, Vector3i& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+        node.convert("z", value.z);
+    }
+};
+
+template <> struct Adaptor<Vector4> {
+    static void pack(Node& node, const Vector4& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+        node.pack("z", value.z);
+        node.pack("w", value.w);
+    }
+    static void convert(const Node& node, Vector4& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+        node.convert("z", value.z);
+        node.convert("w", value.w);
+    }
+};
+
+template <> struct Adaptor<Quaternion> {
+    static void pack(Node& node, const Quaternion& value) {
+        node.pack("x", value.x);
+        node.pack("y", value.y);
+        node.pack("z", value.z);
+        node.pack("w", value.w);
+    }
+    static void convert(const Node& node, Quaternion& value) {
+        node.convert("x", value.x);
+        node.convert("y", value.y);
+        node.convert("z", value.z);
+        node.convert("w", value.w);
+    }
+};
+
+template <typename T> struct Container {
+    static void pack(Node& node, const std::string& key, const T& value) {
+        auto child = node.insert(key);
+        Adaptor<T>::pack(child, value);
+    }
+    static void convert(const Node& node, const std::string& key, T& value, bool required) {
+        auto child = node.child(key);
+        if (!child) {
+            if (!required) {
+                return;
+            }
+            throw std::invalid_argument(fmt::format("Missing property: {}/{}", node.breadcrumbs(), key));
+        } else {
+            Adaptor<T>::convert(child, value);
+        }
+    }
+};
+
+template <typename T> struct Container<std::vector<T>> {
+    static void pack(Node& node, const std::string& key, const std::vector<T>& value) {
+        for (const auto& item : value) {
+            auto child = node.insert(key);
+            Adaptor<T>::pack(child, item);
+        }
+    }
+    static void convert(const Node& node, const std::string& key, std::vector<T>& value, bool required) {
+        (void)required;
+        auto child = node.child(key);
+        while (child) {
+            Adaptor<T>::convert(child, value.emplace_back());
+            child = child.next(key);
+        }
+    }
+};
+
+template <typename T> struct Container<std::list<T>> {
+    static void pack(Node& node, const std::string& key, const std::list<T>& value) {
+        for (const auto& item : value) {
+            auto child = node.insert(key);
+            Adaptor<T>::pack(child, item);
+        }
+    }
+    static void convert(const Node& node, const std::string& key, std::list<T>& value, bool required) {
+        (void)required;
+        auto child = node.child(key);
+        while (child) {
+            Adaptor<T>::convert(child, value.emplace_back());
+            child = child.next(key);
+        }
+    }
+};
+
+template <typename T> struct Container<std::optional<T>> {
+    static void pack(Node& node, const std::string& key, const std::optional<T>& value) {
+        if (value) {
+            auto child = node.insert(key);
+            Adaptor<T>::pack(child, *value);
+        }
+    }
+    static void convert(const Node& node, const std::string& key, std::optional<T>& value, bool required) {
+        (void)required;
+        auto child = node.child(key);
+        if (child && !child.empty()) {
+            value = T{};
+            Adaptor<T>::convert(child, *value);
+        }
+    }
+};
+
+template <typename T> struct Container<std::unordered_map<std::string, T>> {
+    static void pack(Node& node, const std::string& key, const std::unordered_map<std::string, T>& value) {
+        auto child = node.insert(key);
+        for (const auto& pair : value) {
+            auto grandChild = child.insert(pair.first);
+            Adaptor<T>::pack(grandChild, pair.second);
+        }
+    }
+    static void convert(const Node& node, const std::string& key, std::unordered_map<std::string, T>& value,
+                        bool required) {
+        (void)required;
+        auto child = node.child(key);
+        if (child && !child.empty()) {
+            auto grandChild = child.child();
+            while (grandChild) {
+                auto& item = value[std::string{grandChild.name()}];
+                Adaptor<T>::convert(grandChild, item);
+                grandChild = grandChild.next();
+            }
+        }
+    }
+};
+
+template <typename T> inline void pack(Node& node, const std::string& key, const T& value) {
+    Container<T>::pack(node, key, value);
+}
+
+template <typename T> inline void convert(const Node& node, const std::string& key, T& value, bool required) {
+    Container<T>::convert(node, key, value, required);
+}
+
+template <typename T> inline std::string dump(const T& value) {
+    Document doc{Detail::DocumentDefinition<T>::getName(), Detail::DocumentDefinition<T>::getVersion()};
+    value.pack(doc.getRoot());
+    return doc.toString();
+}
+
+template <typename T> inline void load(T& value, const std::string& raw) {
+    Document doc{raw};
+    value.convert(doc.getRoot());
+}
+} // namespace Engine::Xml
+
+#define XML_DEFINE(Type, RootName)                                                                                     \
+    template <> struct Xml::Detail::DocumentDefinition<Type> {                                                         \
+        static const char* getName() {                                                                                 \
+            return RootName;                                                                                           \
+        }                                                                                                              \
+        static const char* getVersion() {                                                                              \
+            return "1.0";                                                                                              \
+        }                                                                                                              \
+    }
+
+#define XML_DEFINE_ENUM_PACK(Field)                                                                                    \
+    if (value == T::Field) {                                                                                           \
+        node.setText(#Field);                                                                                          \
+        return;                                                                                                        \
+    }
+
+#define XML_DEFINE_ENUM_CONVERT(Field)                                                                                 \
+    if (text == #Field) {                                                                                              \
+        value = T::Field;                                                                                              \
+        return;                                                                                                        \
+    }
+
+#define XML_DEFINE_ENUM(Type, ...)                                                                                     \
+    template <> struct Xml::Adaptor<Type> {                                                                            \
+        using T = Type;                                                                                                \
+        static void pack(Node& node, const Type& value) {                                                              \
+            FOR_EACH(XML_DEFINE_ENUM_PACK, __VA_ARGS__)                                                                \
+        }                                                                                                              \
+        static void convert(const Node& node, Type& value) {                                                           \
+            const auto text = node.getText();                                                                          \
+            FOR_EACH(XML_DEFINE_ENUM_CONVERT, __VA_ARGS__)                                                             \
+            node.throwWith(fmt::format("unknown enum value: \"{}\"", text));                                           \
+        }                                                                                                              \
+    }
---
+++ test/engine/utils/xml_tests.cpp
@@ -0,0 +1,423 @@
+#include "../../common.hpp"
+#include <engine/utils/xml.hpp>
+
+using namespace Engine;
+
+TEST_CASE("Write xml document with empty root", "[xml]") {
+    Xml::Document doc{"root", "1.0"};
+    const auto str = doc.toString();
+
+    static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<root/>
+)";
+
+    REQUIRE(str == expected);
+}
+
+struct DocFoo {
+    struct Nested {
+        int value;
+        bool flag;
+
+        void convert(const Xml::Node& xml) {
+            xml.convert("value", value);
+            xml.convert("flag", flag);
+        }
+
+        void pack(Xml::Node& xml) const {
+            xml.pack("value", value);
+            xml.pack("flag", flag);
+        }
+    };
+
+    std::vector<std::string> messages;
+    Vector3i vec;
+    Nested nested;
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("message", messages);
+        xml.convert("vec", vec);
+        xml.convert("nested", nested);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("message", messages);
+        xml.pack("vec", vec);
+        xml.pack("nested", nested);
+    }
+};
+
+XML_DEFINE(DocFoo, "foo");
+
+TEST_CASE("Write and read struct document", "[xml]") {
+    DocFoo foo{};
+    foo.messages = {"Hello World!", "Another message!"};
+    foo.vec = Vector3i{3, 42, 0};
+    foo.nested.value = 99;
+    foo.nested.flag = true;
+
+    const auto str = Xml::dump(foo);
+
+    static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <message>Hello World!</message>
+  <message>Another message!</message>
+  <vec>
+    <x>3</x>
+    <y>42</y>
+    <z>0</z>
+  </vec>
+  <nested>
+    <value>99</value>
+    <flag>true</flag>
+  </nested>
+</foo>
+)";
+
+    REQUIRE(str == expected);
+
+    DocFoo parsed{};
+    Xml::load(parsed, str);
+    REQUIRE(parsed.messages.size() == 2);
+    REQUIRE(parsed.messages[0] == foo.messages[0]);
+    REQUIRE(parsed.messages[1] == foo.messages[1]);
+    REQUIRE(parsed.nested.value == foo.nested.value);
+    REQUIRE(parsed.nested.flag == foo.nested.flag);
+    REQUIRE(parsed.vec == foo.vec);
+}
+
+TEST_CASE("Xml throw if unable to parse property", "[xml]") {
+    SECTION("Missing property") {
+        static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <vec>
+    <x>3</x>
+  </vec>
+  <nested>
+    <value>99</value>
+    <flag>true</flag>
+  </nested>
+</foo>
+)";
+
+        DocFoo foo{};
+        REQUIRE_THROWS_WITH(Xml::load(foo, raw), "Missing property: foo/vec/y");
+    }
+
+    SECTION("Invalid integer property") {
+        static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<foo>
+  <vec>
+    <x>3</x>
+    <y>aaaaa</y>
+    <z>3</z>
+  </vec>
+  <nested>
+    <value>99</value>
+    <flag>true</flag>
+  </nested>
+</foo>
+)";
+
+        DocFoo foo{};
+        REQUIRE_THROWS_WITH(Xml::load(foo, raw), "Unable to parse property: foo/vec/y error: invalid int32_t value");
+    }
+}
+
+struct DocBaz {
+    struct Nested {
+        std::string msg;
+        int value;
+
+        void convert(const Xml::Node& xml) {
+            xml.convert("msg", msg);
+            xml.convert("value", value);
+        }
+
+        void pack(Xml::Node& xml) const {
+            xml.pack("msg", msg);
+            xml.pack("value", value);
+        }
+    };
+
+    std::vector<Nested> items;
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("item", items);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("item", items);
+    }
+};
+
+XML_DEFINE(DocBaz, "baz");
+
+TEST_CASE("Complex nested xml document", "[xml]") {
+    DocBaz baz{};
+    baz.items = {
+        DocBaz::Nested{"Hello World!", 10},
+        DocBaz::Nested{"Some message!", 42},
+    };
+
+    const auto str = Xml::dump(baz);
+
+    static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<baz>
+  <item>
+    <msg>Hello World!</msg>
+    <value>10</value>
+  </item>
+  <item>
+    <msg>Some message!</msg>
+    <value>42</value>
+  </item>
+</baz>
+)";
+
+    REQUIRE(str == expected);
+}
+
+struct DocWithOptional {
+    struct Nested {
+        std::string msg;
+
+        void convert(const Xml::Node& xml) {
+            xml.convert("msg", msg);
+        }
+
+        void pack(Xml::Node& xml) const {
+            xml.pack("msg", msg);
+        }
+    };
+
+    std::optional<Nested> value;
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("value", value);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("value", value);
+    }
+};
+
+XML_DEFINE(DocWithOptional, "doc");
+
+TEST_CASE("Xml with optional container", "[xml]") {
+    SECTION("Dump with optional value present") {
+        DocWithOptional doc{};
+        doc.value = DocWithOptional::Nested{"Hello World!"};
+
+        const auto str = Xml::dump(doc);
+
+        static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <value>
+    <msg>Hello World!</msg>
+  </value>
+</doc>
+)";
+
+        REQUIRE(str == expected);
+    }
+
+    SECTION("Load with optional value present") {
+        static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <value>
+    <msg>Hello World!</msg>
+  </value>
+</doc>
+)";
+
+        DocWithOptional doc{};
+        Xml::load(doc, raw);
+
+        REQUIRE(doc.value.has_value());
+        REQUIRE(doc.value.value().msg == "Hello World!");
+    }
+
+    SECTION("Dump with optional value missing") {
+        DocWithOptional doc{};
+        doc.value = std::nullopt;
+
+        const auto str = Xml::dump(doc);
+
+        static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc/>
+)";
+
+        REQUIRE(str == expected);
+    }
+
+    SECTION("Load with optional value missing") {
+        static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc/>
+)";
+
+        DocWithOptional doc{};
+        Xml::load(doc, raw);
+
+        REQUIRE(!doc.value.has_value());
+    }
+
+    SECTION("Load with optional value missing empty element") {
+        static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <value/>
+</doc>
+)";
+
+        DocWithOptional doc{};
+        Xml::load(doc, raw);
+
+        REQUIRE(!doc.value.has_value());
+    }
+}
+
+struct DocWithMap {
+    struct Nested {
+        std::string msg;
+
+        void convert(const Xml::Node& xml) {
+            xml.convert("msg", msg);
+        }
+
+        void pack(Xml::Node& xml) const {
+            xml.pack("msg", msg);
+        }
+    };
+
+    std::unordered_map<std::string, Nested> items;
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("items", items);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("items", items);
+    }
+};
+
+XML_DEFINE(DocWithMap, "doc");
+
+TEST_CASE("Xml with unordered_map", "[xml]") {
+    DocWithMap doc{};
+    doc.items = {
+        {"first", {"Hello World 1!"}},
+        {"second", {"Hello World 2!"}},
+        {"third", {"Hello World 3!"}},
+    };
+
+    const auto str = Xml::dump(doc);
+
+    static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <items>
+    <second>
+      <msg>Hello World 2!</msg>
+    </second>
+    <third>
+      <msg>Hello World 3!</msg>
+    </third>
+    <first>
+      <msg>Hello World 1!</msg>
+    </first>
+  </items>
+</doc>
+)";
+
+    REQUIRE(str == expected);
+
+    DocWithMap parsed{};
+    Xml::load(parsed, str);
+
+    REQUIRE(parsed.items.size() == doc.items.size());
+    REQUIRE(parsed.items.find("first") != parsed.items.end());
+    REQUIRE(parsed.items.find("second") != parsed.items.end());
+    REQUIRE(parsed.items.find("third") != parsed.items.end());
+    REQUIRE(parsed.items["first"].msg == doc.items["first"].msg);
+    REQUIRE(parsed.items["second"].msg == doc.items["second"].msg);
+    REQUIRE(parsed.items["third"].msg == doc.items["third"].msg);
+}
+
+struct DocWithEnum {
+    enum class SomeEnum {
+        None,
+        Hello,
+        World,
+    };
+
+    SomeEnum value{SomeEnum::None};
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("value", value);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("value", value);
+    }
+};
+
+XML_DEFINE(DocWithEnum, "doc");
+XML_DEFINE_ENUM(DocWithEnum::SomeEnum, None, Hello, World);
+
+TEST_CASE("Xml with enum", "[xml]") {
+    DocWithEnum doc{};
+    doc.value = DocWithEnum::SomeEnum::Hello;
+
+    const auto str = Xml::dump(doc);
+
+    static const std::string expected = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <value>Hello</value>
+</doc>
+)";
+
+    REQUIRE(str == expected);
+
+    DocWithEnum parsed{};
+    Xml::load(parsed, str);
+
+    REQUIRE(parsed.value == doc.value);
+}
+
+struct DocWithNonRequired {
+    struct Nested {
+        std::string msg;
+
+        void convert(const Xml::Node& xml) {
+            xml.convert("msg", msg, false);
+        }
+
+        void pack(Xml::Node& xml) const {
+            xml.pack("msg", msg);
+        }
+    };
+
+    Nested nested;
+
+    void convert(const Xml::Node& xml) {
+        xml.convert("nested", nested);
+    }
+
+    void pack(Xml::Node& xml) const {
+        xml.pack("nested", nested);
+    }
+};
+
+XML_DEFINE(DocWithNonRequired, "doc");
+
+TEST_CASE("Xml with optional property", "[xml]") {
+    static const std::string raw = R"(<?xml version="1.0" encoding="utf-8"?>
+<doc>
+  <nested/>
+</doc>
+)";
+
+    DocWithNonRequired parsed{};
+    Xml::load(parsed, raw);
+
+    REQUIRE(parsed.nested.msg.empty());
+}
